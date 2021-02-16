71be1d1de2327302e2a8b4b9e7ea3d4c5f54dae4
muhammad-sohel131 <73376980+muhammad-sohel131@users.noreply.github.com>
2021-02-16 23:55:26 +0600

    Update app.js

--- app.js
+++ app.js
@@ -21,27 +21,27 @@ submitBtn.addEventListener('click', function(){
             herName = 'Mia Khalifa'
             wow= 'Great! You are so lucky'
         }
-        else if(nameValue=='emo' || nameValue == 'sha' || nameValue=='sah'){
+        else if(nameValue=='emo' || nameValue == 'sha' || nameValue=='sah' || nameValue=='Emo' || nameValue == 'Sha' || nameValue=='Sah'){
             herName = 'Suny Leon'
             wow= 'Great! You are so lucky'
         } 
-        else if(nameValue=='soh' || nameValue=='sho') herName='Mim'
+        else if(nameValue=='soh' || nameValue=='sho' || nameValue=='Soh' || nameValue=='Sho') herName='Mim'
         else if(nameValue=='sop'  || nameValue=='Sop') herName='Sumaiya'
-        else if(nameValue=='rit') herName=='There is one who'
+        else if(nameValue=='rit' || nameValue=='Rit') herName=='There is one who'
         else {
         herName='There is no one who'
          wow = 'Ops! So Sad.'
-         let loves = 0
+         let loves == 0
     }
     }else{
         if(nameValue=='far' || nameValue=='Far') herName='Shuvo' 
-        else if(nameValue=='mim' || nameValue=='sal') herName= 'Sohel'
-        else if(nameValue=='sum') herName='Sopon'
+        else if(nameValue=='mim' || nameValue=='sal' || nameValue=='Mim' || nameValue=='Sal') herName= 'Sohel'
+        else if(nameValue=='sum' nameValue=='Sum') herName='Sopon'
         else if(nameValue=='rit') herName='There is one who'
         else {
             herName='There is no one who'
              wow = 'Ops! So Sad.'
-             let loves = 0
+             let loves == 0
         }
     }
     
